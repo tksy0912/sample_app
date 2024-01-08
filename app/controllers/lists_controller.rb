f1c624b5ef812e87cc1d6e931b99807d1839ef53
EC2 Default User <ec2-user@ip-172-31-15-227.ap-northeast-1.compute.internal>
2024-01-08 06:12:07 +0000

    [Finish] 3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -33,8 +33,7 @@ class ListsController < ApplicationController
   end
 
   private
-  # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
